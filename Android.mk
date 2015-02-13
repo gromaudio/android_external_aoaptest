a0e1200b5964745c053f2b89b9aa88ff02f9d45c
Ivan Zaitsev <ivan.zaitsev@gmail.com>
2015-02-13 15:04:26 -0800

    First commit.

---
+++ Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= aoap_test.c
+
+LOCAL_MODULE:= aoap_test
+
+LOCAL_CFLAGS :=
+
+LOCAL_C_INCLUDES += \
+        external/libusb
+
+LOCAL_SHARED_LIBRARIES := \
+        libusb
+
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_EXECUTABLE)
